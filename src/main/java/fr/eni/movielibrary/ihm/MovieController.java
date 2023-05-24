270f170712383e66d618a62e61d92301be13fed4
julienaverbouch <julienaverbouch@gmail.com>
2023-05-24 11:27:53 +0200

    init

---
+++ src/main/java/fr/eni/movielibrary/ihm/MovieController.java
@@ -0,0 +1,19 @@
+package fr.eni.movielibrary.ihm;
+
+import java.util.List;
+
+import fr.eni.movielibrary.bo.Movie;
+
+public class MovieController {
+
+	public Movie findMovie(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Movie> showAllMovies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
